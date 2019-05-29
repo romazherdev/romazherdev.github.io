f617db8dd46fa6ac002233595d5bac29f266995e
Romazherdev <romazherdev@gmail.com>
2019-05-29 22:20:27 +0300

    the website is reworked

---
+++ precache-manifest.573ce30c1d7dea60b9fecef71569721e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dba3d7b184fc93aaabdc",
+    "url": "/static/js/main.3dc2c2c5.chunk.js"
+  },
+  {
+    "revision": "014629d5936ee6cb1c50",
+    "url": "/static/js/2.0d19a93f.chunk.js"
+  },
+  {
+    "revision": "dba3d7b184fc93aaabdc",
+    "url": "/static/css/main.7cae08fb.chunk.css"
+  },
+  {
+    "revision": "14a0285a82194407de0bcfea9a3f4ec4",
+    "url": "/index.html"
+  }
+];
