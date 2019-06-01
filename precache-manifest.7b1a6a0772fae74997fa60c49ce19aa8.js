b9dbfb8461b12068a134c1e77760299cc47f0d49
Romazherdev <romazherdev@gmail.com>
2019-06-01 20:57:12 +0300

    routing fixes

---
+++ precache-manifest.7b1a6a0772fae74997fa60c49ce19aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bad6ea030ab0bf225983",
+    "url": "/static/js/main.c7cae63a.chunk.js"
+  },
+  {
+    "revision": "014629d5936ee6cb1c50",
+    "url": "/static/js/2.0d19a93f.chunk.js"
+  },
+  {
+    "revision": "bad6ea030ab0bf225983",
+    "url": "/static/css/main.83a0a5b0.chunk.css"
+  },
+  {
+    "revision": "aaebe0692c196b3b16ffab10a3515732",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3cfccb22d96a292afeecac174d59284.js"
+  "/precache-manifest.7b1a6a0772fae74997fa60c49ce19aa8.js"
 );
 
 workbox.clientsClaim();
