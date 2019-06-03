130d888022136a469a7afc94baac8965f80abd61
Romazherdev <romazherdev@gmail.com>
2019-06-03 21:13:25 +0300

    pens

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b1a6a0772fae74997fa60c49ce19aa8.js"
+  "/precache-manifest.c27ea9b1f3d4a3d23c03e59cd0a321dd.js"
 );
 
 workbox.clientsClaim();
